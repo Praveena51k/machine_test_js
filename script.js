6e5d68b3cf3c243785531e693aa41172907a0a4d
praveena <praveena51k@gmail.com>
2022-06-20 11:10:24 +0530

    last commit

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function buttonClick(val)
 {
     document.getElementById("screen").value+=val
+    
+}
+function evalClick(val)
+{
+    value1 = parseInt(document.getElementById("screen").value)
+    operator = val
+    document.getElementById("screen").value = ""
+
 }
 
 
@@ -12,7 +20,25 @@ function clearDisplay()
 
 function equalClick()
 {
-    var text = document.getElementById("screen").value
-    var result = eval(text)
-    document.getElementById("screen").value = result
+   value2 = parseInt(document.getElementById("screen").value)
+   switch(operator){
+    case '+':
+        // result = value1 + value2;
+        document.getElementById("screen").value=value1 + value2;
+        break;
+    case '-':
+        // result = value1 - value2
+        document.getElementById("screen").value=value1 - value2;
+        break;
+    case '*':
+        // result = value1 * value2
+        document.getElementById("screen").value=value1 * value2;
+        break;
+    case '/':
+        // result = value1 / value2
+        document.getElementById("screen").value=value1 / value2;
+        break;
+    
+   }
+
 }
